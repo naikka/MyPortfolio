9ddd159ead559ef4cdc42a7ff5a14d8848652643
naikka <micahgersava20@gmail.com>
2024-08-28 23:40:24 +0800

    Tailwind Setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
+import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-    </Routes>
-    </Router>
+    <div>
+      <Navbar/>
+
+    </div>
     
   );
 }
